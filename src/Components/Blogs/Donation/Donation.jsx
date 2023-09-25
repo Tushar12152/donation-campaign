66ffce0a5943f208335602d09afc8b33ca7de594
Tushar12152 <tusharimran470@gmail.com>
2023-09-25 00:28:10 -0600

    set item to localstorage successfully

--- src/Components/Blogs/Donation/Donation.jsx
+++ src/Components/Blogs/Donation/Donation.jsx
@@ -1,5 +1,7 @@
 
 const Donation = () => {
+    const donation=JSON.parse(localStorage.getItem('donation'))
+    console.log(donation)
     return (
         <div>
              Donation
--- src/Components/Blogs/Home/Home Card/Donate Details/DonateDetails.jsx
+++ src/Components/Blogs/Home/Home Card/Donate Details/DonateDetails.jsx
@@ -17,11 +17,38 @@ const DonateDetails = () => {
         setDonation(findDonate)
     },[donations,id])
   
-    console.log(donation)
+    // console.log(donation)
        const {image, title, description, text_color: button_color,price}=donation;
 
-       const handleAlert=()=>{
-        swal("Congratulations!", "You Donated!", "success");
+       const handleAddToDonation=()=>{
+       
+
+       const donationList=[];
+       const donationGet=JSON.parse(localStorage.getItem('donation'))
+       if(!donationGet){
+        donationList.push(donation)
+        localStorage.setItem('donation',JSON.stringify(donationList))
+        swal("Congratulations!", "Succesfully Donated!", "success");
+       }
+       else{
+          const isExist=donationGet.find(donate=> donate.id===id)
+
+          if(!isExist){
+            donationList.push(...donationGet,donation)
+            localStorage.setItem('donation',JSON.stringify(donationList))
+            swal("Congratulations!", "Succesfully Donated!", "success");
+          }
+
+          else{
+            swal("Sorry!", "You Already Donated this!  please try Another!!!", "error");
+          }
+       
+       }
+
+        
+        
+        
+      
        }
 
     return (
@@ -32,7 +59,7 @@ const DonateDetails = () => {
             <h1 className="text-[#0B0B0B] text-4xl font-bold  pt-10 pl-10">{title}</h1>
             <p className="pl-10 pt-1 text-[#0B0B0BB3] leading-6">{description}</p>
            <div className="absolute top-80 rounded-lg bg-[#0B0B0B80] w-[1120px] mx-auto py-5   lg:ml-12">
-                 <button onClick={handleAlert} className="py-2 px-4 ml-10 rounded-lg text-white" style={{ backgroundColor: button_color }}> Donate ${price}</button>
+                 <button onClick={handleAddToDonation} className="py-2 px-4 ml-10 rounded-lg text-white" style={{ backgroundColor: button_color }}> Donate ${price}</button>
            </div>
         </div>
     );
