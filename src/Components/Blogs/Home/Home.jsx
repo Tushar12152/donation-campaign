505f0a47881d7487fa8dca48b2ad5dcc5e1bbf5e
Tushar12152 <tusharimran470@gmail.com>
2023-09-25 22:05:02 -0600

    Banner.propTypes = {

--- src/Components/Blogs/Home/Home.jsx
+++ src/Components/Blogs/Home/Home.jsx
@@ -1,19 +1,26 @@
+
+import  { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import HomeCard from "./Home Card/HomeCard";
 import Banner from "../../nav/Banner/Banner";
 
+
 const Home = () => {
-    const donates=useLoaderData()
-    
+    const donates = useLoaderData();
+    const [filteredDonates, setFilteredDonates] = useState(donates);
+
+    const handleSearch = (filteredData) => {
+        setFilteredDonates(filteredData);
+    }
+
     return (
         <div>
-      <Banner></Banner>
-
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-24 w-[90%] mx-auto">
-             {
-                donates.map((donate)=><HomeCard key={donate.id} donate={donate}></HomeCard>)
-             }
-          </div>
+            <Banner donation={donates} onSearch={handleSearch} />
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-24 w-[90%] mx-auto">
+                {
+                    filteredDonates.map((donate) => <HomeCard key={donate.id} donate={donate} />)
+                }
+            </div>
         </div>
     );
 };
