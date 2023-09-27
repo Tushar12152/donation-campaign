288fb301dfc29c7a1a1415ec3cada8b01523495e
Tushar12152 <tusharimran470@gmail.com>
2023-09-26 21:15:55 -0600

    correction banner section overlay

--- src/Components/nav/Banner/Banner.jsx
+++ src/Components/nav/Banner/Banner.jsx
@@ -4,7 +4,7 @@ import  { useState } from 'react';
 
 const Banner = ({ donation, onSearch }) => {
     const overlay = {
-        backgroundColor: 'rgba(0,0,0,0.3)'
+        backgroundColor: 'rgba(0,0,0,0.9)'
     }
 
    const [searchInput, setSearchInput] = useState('');
@@ -16,7 +16,7 @@ const Banner = ({ donation, onSearch }) => {
 
     return (
         <div className="flex flex-col justify-center items-center bg-[url('https://i.ibb.co/wd9j318/384056721-24297713219819684-2982518742680676676-n.jpg')] bg-no-repeat  h-4/6 mt-4 bg-cover bg-blend-multiply bg-white bg-opacity-90" style={overlay} >
-             <h1 className="pt-40 text-center text-[#0B0B0B] font-bold text-[48px]">I Grow By Helping People In Need</h1>
+             <h1 className="text-white pt-40 text-center  font-bold text-[48px]">I Grow By Helping People In Need</h1>
               <div className="pb-40">
                   <input placeholder="Search here...."  className="w-56 rounded-l-lg mx-auto border-2"  type="text"  value={searchInput}  onChange={(e) => setSearchInput(e.target.value)} />
 
